0a61fa723ad66b9a04755923269837e6b4d67573
Anuj Gupta <anugup333@gmail.com>
2025-07-10 19:31:14 +0530

    end

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ss^3y9xih+zz^h460!%jdtfyhl9kke=4@$j*3#^!my4rmt_)(t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
